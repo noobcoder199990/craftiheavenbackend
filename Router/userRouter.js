506fbcb78a757cbabc71a9f2653030628c92d1f9
Shijin <shijin.varghese@cosmictechnologies.in>
2024-04-13 21:26:55 +0530

    d

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -297,6 +297,7 @@ router
             maxAge: ACCESS_TOKEN_EXPIRY_IN_MINUTES * 3600000,
             httpOnly: false,
             secure: JSON.parse(false),
+            SameSite: "None",
           });
           log.debug(259);
           const { first_name, last_name, email, _id } = user;
